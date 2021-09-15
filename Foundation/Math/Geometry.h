556c6d676656077dc37ce3eb5775c469e27dce3f
goteet <goteet@qq.com>
2021-09-15 17:47:12 +0800

    add cube definition.

--- Foundation/Math/Geometry.h
+++ Foundation/Math/Geometry.h
@@ -87,6 +87,32 @@ namespace math
         vector_t<value_type, EDim::_3> _normal;
     };
 
+    template<typename value_type>
+    class cube
+    {
+    public:
+        cube(const point<value_type, EDim::_3>& c, const vector_t<value_type, EDim::_3>& e) :_center(c) { set_extends(e); }
+        cube(const point<value_type, EDim::_3>& c, const vector_t<value_type, EDim::_3>& e, normalize_hint) : _center(c), _extends(e) {  }
+        void set_width(value_type w) { _extends.x = max2(w, value_type(0)); }
+        void set_height(value_type h) { _extends.y = max2(h, value_type(0)); }
+        void set_depth(value_type d) { _extends.z = max2(d, value_type(0)); }
+        void set_extends(value_type w, value_type h, value_type d) { set_width(w); set_height(h); set_depth(d); }
+        void set_extends(const vector_t<value_type, EDim::_3>& e) { set_width(e.x); set_height(e.y); set_depth(e.z); }
+        void set_extends(const vector_t<value_type, EDim::_3>& e, normalize_hint) { _extends = e; }
+        const point<value_type, EDim::_3>& center() const { return _center; }
+        point<value_type, EDim::_3>& center() { return _center; }
+        const vector_t<value_type, EDim::_3>& extends() const { return _extends; }
+        vector_t<value_type, EDim::_3> axis_x() const { return vector_t<value_type, EDim::_3>::unit_x(); }
+        vector_t<value_type, EDim::_3> axis_y() const { return vector_t<value_type, EDim::_3>::unit_y(); }
+        vector_t<value_type, EDim::_3> axis_z() const { return vector_t<value_type, EDim::_3>::unit_z(); }
+        value_type width() const { return _extends.x; }
+        value_type height() const { return _extends.y; }
+        value_type depth() const { return _extends.z; }
+    private:
+        point<value_type, EDim::_3> _center;
+        vector_t<value_type, EDim::_3> _extends;
+    };
+
 
     enum class intersection : unsigned char
     {
@@ -96,7 +122,7 @@ namespace math
     };
 
     template<typename value_type>
-    intersection intersect(const ray<value_type, EDim::_3>& ray,
+    intersection intersect_plane(const ray<value_type, EDim::_3>& ray,
         const point<value_type, EDim::_3>& pos, const vector_t<value_type, EDim::_3>& normal, bool dualface,
         value_type& t)
     {
@@ -118,14 +144,14 @@ namespace math
         const plane<value_type>& plane, dual_face_hint,
         value_type& t)
     {
-        return intersect(ray, plane.position(), plane.normal(), true, t);
+        return intersect_plane(ray, plane.position(), plane.normal(), true, t);
     }
 
     template<typename value_type>
     intersection intersect(const ray<value_type, EDim::_3>& ray,
         const plane<value_type>& plane, value_type& t)
     {
-        return intersect(ray, plane.position(), plane.normal(), false, t);
+        return intersect_plane(ray, plane.position(), plane.normal(), false, t);
     }
 
     template<typename value_type>
@@ -162,6 +188,153 @@ namespace math
             t0 = t1 = -b * value_type(0.5) / a;
             return intersection::tangent;
         }
+    }
+
+    template<typename value_type>
+    intersection intersect_cube(const ray<value_type, EDim::_3>& ray,
+        const vector_t<value_type, EDim::_3>& position,
+        const vector_t<value_type, EDim::_3>& axis_x,
+        const vector_t<value_type, EDim::_3>& axis_y,
+        const vector_t<value_type, EDim::_3>& axis_z,
+        value_type extend_x, value_type extend_y, value_type extend_z,
+        value_type& t0, value_type& t1,
+        vector_t<value_type, EDim::_3>& n0,
+        vector_t<value_type, EDim::_3>& n1)
+    {
+        int parallelMask = 0;
+        bool found = false;
+
+        vector_t<value_type, EDim::_3> dirDotAxis;
+        vector_t<value_type, EDim::_3> ocDotAxis;
+        vector_t<value_type, EDim::_3> oc = position - ray.origin();
+        vector_t<value_type, EDim::_3> axis[3] = { axis_x, axis_y, axis_z };
+        value_type extend[3] = { extend_x, extend_y, extend_z };
+
+        for (int i = 0; i < 3; ++i)
+        {
+            dirDotAxis[i] = dot(ray.direction(), axis[i]);
+            ocDotAxis[i] = dot(oc, axis[i]);
+
+            if (dirDotAxis[i] == value_type(0))
+            {
+                parallelMask |= 1 << i;
+            }
+            else
+            {
+                value_type extend_i = (dirDotAxis[i] > value_type(0)) ? extend[i] : -extend[i];
+                value_type invDA = value_type(1) / dirDotAxis[i];
+
+                if (!found)
+                {
+                    t0 = (ocDotAxis[i] - extend_i) * invDA;
+                    t1 = (ocDotAxis[i] + extend_i) * invDA;
+                    n1 = n0 = axis[i];
+                    found = true;
+                }
+                else
+                {
+                    value_type s = (ocDotAxis[i] - extend_i) * invDA;
+                    if (s > t0)
+                    {
+                        t0 = s;
+                        n0 = axis[i];
+                    }
+
+                    s = (ocDotAxis[i] + extend_i) * invDA;
+                    if (s < t1)
+                    {
+                        t1 = s;
+                        n1 = axis[i];
+                    }
+
+                    if (t0 > t1)
+                    {
+                        return intersection::none;
+                    }
+                }
+            }
+        }
+
+        if (parallelMask)
+        {
+            for (int i = 0; i < 3; ++i)
+            {
+                if (parallelMask & (1 << i))
+                {
+                    if (fabs(ocDotAxis[i] - t0 * dirDotAxis[i]) > extend[i] ||
+                        fabs(ocDotAxis[i] - t1 * dirDotAxis[i]) > extend[i])
+                    {
+                        return intersection::none;
+                    }
+                }
+            }
+        }
+
+        if (t1 < value_type(0))
+        {
+            return intersection::none;
+        }
+        else if (t0 < value_type(0))
+        {
+            t0 = t1;
+            return intersection::inside;
+        }
+        else
+        {
+            return intersection::intersect;
+        }
+    }
+
+    template<typename value_type>
+    intersection intersect(const ray<value_type, EDim::_3>& ray, const cube<value_type>& _cube,
+        value_type& t0, value_type& t1, vector_t<value_type, EDim::_3>& n0, vector_t<value_type, EDim::_3>& n1)
+    {
+        return intersect_cube(ray, _cube.center(),
+            _cube.axis_x(), _cube.axis_y(), _cube.axis_z(),
+            _cube.width(), _cube.height(), _cube.depth(),
+            t0, t1, n0, n1);
+    }
+
+    template<typename value_type>
+    intersection intersect_triangle(const ray<value_type, EDim::_3>& ray,
+        const point<value_type, EDim::_3>& v0, const point<value_type, EDim::_3>& v1, const point<value_type, EDim::_3>& v2,
+        value_type& t, value_type&u, value_type& v, vector_t<value_type, EDim::_3>& normal)
+    {
+        //moller-trumbore
+        vector_t<value_type, EDim::_3> v1v0 = v1 - v0;
+        vector_t<value_type, EDim::_3> v2v0 = v2 - v0;
+
+        vector_t<value_type, EDim::_3> pv = cross(ray.direction(), v2v0);
+        float det = dot(v1v0, pv);
+        if (det == 0.0f)
+        {
+            return intersection::none;
+        }
+
+        float invDet = 1.0f / det;
+
+        vector_t<value_type, EDim::_3> tv = ray.origin() - v0;
+        u = dot(tv, pv) * invDet;
+        if (u < 0.0f || u > 1.0f)
+        {
+            return intersection::none;
+        }
+
+        vector_t<value_type, EDim::_3> qv = cross(tv, v1v0);
+        v = dot(ray.direction(), qv) * invDet;
+        if (v < 0.0f || u + v > 1.0f)
+        {
+            return intersection::none;
+        }
+
+        t = dot(v2v0, qv) * invDet;
+        if (t < 0.0f)
+        {
+            return intersection::none;
+        }
+
+        normal = cross(v2v0, v1v0).normalized();
+        return intersection::intersect;
     }
 
     template<typename value_type> using point2d = point<value_type, EDim::_2>;
