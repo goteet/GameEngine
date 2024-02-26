33b0cf0223ed32a3447023193938c20ba7220cc0
x5lcfd <xingouy@gmail.com>
2024-02-26 21:36:43 +0800

    add d3d12 header files.

--- Engine/Source/PreIncludeFilesWindows.h
+++ Engine/Source/PreIncludeFilesWindows.h
@@ -1,8 +1,16 @@
 #pragma once
 // TODO: We should include platform header firstly, which system related APIs need this.
 #define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
 #include <Windows.h>
 
 #include <d3d11_3.h>
 #include <dxgi1_3.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#ifdef _DEBUG
+	#include <dxgidebug.h>
+#endif
 #include <wrl/client.h>
