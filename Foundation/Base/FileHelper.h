47b499286ca406d646e221ad9d0aac73d4089f52
goteet <goteet@qq.com>
2025-10-09 20:38:36 +0800

    archive & serialization

--- Foundation/Base/FileHelper.h
+++ Foundation/Base/FileHelper.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <string>
 #include <vector>
+
 #include "ScopeHelper.h"
+#include "Serialization.h"
 
 namespace base
 {
@@ -22,7 +24,8 @@ namespace base
 
     bool read_entire_file(const std::string& path, file_buffer& buffer)
     {
-        FILE* f = fopen(path.c_str(), "rb");
+        FILE* f = nullptr;
+        fopen_s(&f, path.c_str(), "rb");
         if (f != nullptr)
         {
             ON_EXIT{ fclose(f); };
@@ -46,4 +49,68 @@ namespace base
         }
         return false;
     }
+
+    struct file_archive : public base_archive
+    {
+        virtual ~file_archive() { fclose(file); }
+        virtual bool is_at_end() { return false; }
+
+        void destroy() { delete this; }
+
+    protected:
+        file_archive(FILE* f) : file(f) { };
+        FILE* file = nullptr;
+
+        file_archive(file_archive& rhs) = delete;
+        file_archive(file_archive&& rhs) = delete;
+    };
+
+
+    struct file_archive_read : public file_archive
+    {
+        virtual bool is_saving() sealed override { return false; }
+        virtual base_archive& serialize(void* data, uint32_t length) override { fread(data, 1, length, file); return *this; }
+
+        friend file_archive* create_archive_file_read(const std::string& path);
+
+    protected:
+        file_archive_read(FILE* f) : file_archive(f) { }
+    };
+
+    struct file_archive_write : public file_archive
+    {
+        virtual bool is_saving() sealed override { return true; }
+        virtual base_archive& serialize(void* data, uint32_t length) override { fwrite(data, 1, length, file); return *this; }
+        virtual bool is_at_end() override { return file_length > 0 && file_length <= ftell(file); }
+
+        friend file_archive* create_archive_file_write(const std::string& path);
+
+    protected:
+        file_archive_write(FILE* f) : file_archive(f)
+        {
+            fseek(f, 0, SEEK_END);
+            file_length = ftell(f);
+            fseek(f, 0, SEEK_SET);
+        }
+
+    private:
+        size_t file_length = 0;
+    };
+
+
+    file_archive* create_archive_file_read(const std::string& path)
+    {
+        FILE* f = nullptr;
+        errno_t err = fopen_s(&f, path.c_str(), "rb");
+        return (err == 0) ? new file_archive_read(f) : nullptr;
+    }
+
+    file_archive* create_archive_file_write(const std::string& path)
+    {
+        FILE* f = nullptr;
+        errno_t err = fopen_s(&f, path.c_str(), "wb");
+        return (err == 0) ? new file_archive_write(f) : nullptr;
+    }
+
+
 }
--- Foundation/Dummy.cpp
+++ Foundation/Dummy.cpp
@@ -9,3 +9,32 @@ int MathLibraryDummy()
     return 0;
 
 }
+
+
+#include "Serialization.h"
+#include "FileHelper.h"
+
+
+struct A
+{
+    int a = 0;
+
+    A(int x = 5) :a(x) {}
+    A(A&) = default;
+};
+
+int serialization_dummy()
+{
+    std::vector<A> saved_array = {A(), A(), A(), A()};
+    int length = static_cast<int>(saved_array.size());
+
+    std::string file_path = std::string("e://test.txt");
+    base::file_archive* archive = base::create_archive_file_write(file_path);
+
+    A a;
+    *archive << a;
+
+    archive->destroy();
+
+    return 0;
+}
